1b7a3346f30a2e36970da35a8ae5bd8608abbd40
kasaco223 <caescobarl@outlook.com>
2025-07-10 09:36:54 -0500

    Actualización de lanzar-pilatos.sh y mogdata.db

--- lanzar-pilatos.sh
+++ lanzar-pilatos.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 cd /home/nea/PilatosEventEntry
-node init-db.js
+
+# Iniciar el backend primero
+node serve.js &
+
+# Iniciar el frontend
 npm run dev &
+
 sleep 8
-firefox --kiosk http://localhost:3000 
+
+while true; do
+    # Verifica si Firefox está corriendo
+    if pgrep firefox > /dev/null; then
+        echo "Firefox ya está abierto."
+        break
+    else
+        echo "Firefox no está abierto. Intentando abrirlo..."
+        firefox --kiosk http://localhost:3000 &
+    fi
+    sleep 5
+done 
